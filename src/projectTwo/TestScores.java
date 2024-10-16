b13cc3fd660007bbfdd362487ec0afe46fb0e0ab
Oij13 <oij13@icloud.com>
2024-10-16 14:03:27 -0400

    Project 1 finished

--- src/projectTwo/TestScores.java
+++ src/projectTwo/TestScores.java
@@ -10,20 +10,73 @@ public class TestScores {
         int SIZE = rand.nextInt(3,10);
 
         int[] testScores = new int[SIZE];
+        char[] letterGrades = new char[SIZE];
+
+        System.out.printf("Number of test scores: %d\n",SIZE);
+
+        for (int i = 0; i < SIZE; i++){
+            System.out.println("Enter test score "+(i+1)+":");
+            testScores[i] = scn.nextInt();
+        }
+
+        for (int i = 0; i < SIZE; i++){
+            letterGrades[i] = getLetterGrade(testScores[i]);
+        }
+
+        printGrades(testScores, letterGrades);
+        printHighestScore(testScores);
+        printLowestSCore(testScores);
+        printAverageScore(testScores);
     }
-    public void getLetterGrade(){
+    public static char getLetterGrade(int score){
+        if (score >=90) {
+            return 'A';
+        }
+        else if (score >= 80){
+            return 'B';
+        }
+        else if (score >= 70){
+            return 'C';
+        }
+        else if (score >= 60){
+            return 'D';
+        }
+        else {
+            return 'F';
+        }
+    }
+    public static void printGrades(int[] scores, char[] grades){
+        System.out.printf("%-10s %-5s%n","Score","Grade");
+        for (int i = 0; i < scores.length; i++){
+            System.out.printf("%-10d %-5c%n", scores[i], grades[i]);
+        }
+    }
+    public static void printHighestScore(int[] scores){
+        int highest = scores[0];
+        for (int score : scores) {
+            if (score > highest){
+                highest = score;
+            }
+        }
+        System.out.println("Highest Score: " + highest);
+    }
+    public static void printLowestSCore(int[] scores){
+        int lowest = scores[0];
+        for(int score : scores){
+            if(score < lowest){
+                lowest = score;
+            }
+        }
+        System.out.println("Lowest Score: " + lowest);
 
     }
-    public void printGrades(){
+    public static void printAverageScore(int[] scores){
 
-    }
-    public void printHighestScore(){
-
-    }
-    public void printLowestSCore(){
-
-    }
-    public void printAverageScore(){
-
+        int total = 0;
+        for (int score: scores){
+            total += score;
+        }
+        double average = (double) total / scores.length;
+        System.out.println("Average Score: " + average);
     }
 }
