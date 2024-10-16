5d08e779cf71c29f7a6add66bebb27a3a8bec81b
Oij13 <oij13@icloud.com>
2024-10-16 09:44:11 -0400

    Project 1 finished

--- src/projectTwo/TestScores.java
+++ src/projectTwo/TestScores.java
@@ -1,4 +1,29 @@
 package projectTwo;
 
+import java.util.Random;
+import java.util.Scanner;
 public class TestScores {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        Scanner scn = new Scanner(System.in);
+
+        int SIZE = rand.nextInt(3,10);
+
+        int[] testScores = new int[SIZE];
+    }
+    public void getLetterGrade(){
+
+    }
+    public void printGrades(){
+
+    }
+    public void printHighestScore(){
+
+    }
+    public void printLowestSCore(){
+
+    }
+    public void printAverageScore(){
+
+    }
 }
