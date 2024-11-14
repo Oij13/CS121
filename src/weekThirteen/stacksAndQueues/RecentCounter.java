e322d8a060d87a47f357efeda560874df941becd
Oij13 <oij13@icloud.com>
2024-11-13 22:05:27 -0500

    Project 1 finished

--- src/weekThirteen/stacksAndQueues/RecentCounter.java
+++ src/weekThirteen/stacksAndQueues/RecentCounter.java
@@ -2,5 +2,15 @@ package weekThirteen.stacksAndQueues;
 import java.util.LinkedList;
 import java.util.Queue;
 public class RecentCounter {
+    private Queue<Integer> queue = new LinkedList<>();
+    public RecentCounter(){
+    }
+    public int ping(int t){
+        queue.add(t);
+        while (queue.peek() < (t - 3000)) {
+            queue.poll();
+        }
+        return queue.size();
+    }
 
 }
--- src/weekThirteen/stacksAndQueues/Test.java
+++ src/weekThirteen/stacksAndQueues/Test.java
@@ -3,9 +3,14 @@ package weekThirteen.stacksAndQueues;
 public class Test {
     public static void main(String[] args) {
         ValidParenthesis valid = new ValidParenthesis();
+        RecentCounter counter =  new RecentCounter();
 
         System.out.println(valid.isValid("({{}})"));
         System.out.println(valid.isValid("([)]"));
+        System.out.println(counter.ping(1));
+        System.out.println(counter.ping(100));
+        System.out.println(counter.ping(3001));
+        System.out.println(counter.ping(3002));
 
     }
 }
