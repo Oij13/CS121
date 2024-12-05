28a6827f8d06d067c3005422d42dadb8bf673393
Oij13 <oij13@icloud.com>
2024-12-05 15:12:35 -0500

    Project 1 finished

---
+++ src/weekFifteen/XMLActivity/BankXML.java
@@ -0,0 +1,37 @@
+package weekFifteen.XMLActivity;
+import java.io.File;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
+public class BankXML {
+    public static void main(String[] args) {
+        try {
+            File input = new File("BankAccount.xml");
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(input);
+
+            System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
+
+            NodeList bankList = doc.getElementsByTagName("bank");
+            for (int i = 0; i < bankList.getLength(); i++){
+                Node node = bankList.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE){
+                    Element element = (Element) node;
+                    System.out.println("ID: " + element.getAttribute("id"));
+                    System.out.println("Account Number" + element.getElementsByTagName("accntNum").item(0).getTextContent());
+                    System.out.println("Account Balance" + element.getElementsByTagName("balance").item(0).getTextContent());
+                    System.out.println("Account Type" + element.getElementsByTagName("accntType").item(0).getTextContent());
+                    System.out.println();
+                }
+            }
+        } catch (Exception e){
+            System.out.println("Error reading the XML file: " + e.getMessage());;
+        }
+
+    }
+}
